b4686b8d362ed18fdc625f15c7f5dcee20b60184
Lucas Barbieri <lucas.bertani@pucpr.edu.br>
2022-09-26 15:12:33 -0300

    somativa 2

---
+++ src/pages/cadastro.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function cadastro() {
+    return (
+        <div className="app">
+
+
+            <h1>Cadastro</h1>
+        </div>
+    )
+}
+
+export default cadastro
---
+++ src/pages/principal.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function principal() {
+    return (
+        <div className="app">
+
+
+            <h1>Página principal</h1>
+        </div>
+    )
+}
+
+export default principal
